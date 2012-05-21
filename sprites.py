5e7ba40649695877b4354dcb959370553627fafa
seth-- <xd.seth@gmail.com>
2012-05-21 23:26:06 +0000

    using spaces for indentation

--- sprites.py
+++ sprites.py
@@ -3,7 +3,7 @@ import pygame, os
 def loadSliced(image, coords):
     sprites = []
     for i in coords:
-	iWidth =  i["right"] - i["left"]
-	iHeight = i["bottom"] - i["top"]
-	sprites.append(pygame.transform.scale(image.subsurface(i["left"], i["top"], iWidth, iHeight), (iWidth * 2, iHeight * 2)))
+        iWidth =  i["right"] - i["left"]
+        iHeight = i["bottom"] - i["top"]
+        sprites.append(pygame.transform.scale(image.subsurface(i["left"], i["top"], iWidth, iHeight), (iWidth * 2, iHeight * 2)))
     return sprites
